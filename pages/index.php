23c9e84399af8e473e02cfdca9fd5e3384e566e4
changco0987 <williamchangco26@gmail.com>
2022-07-24 17:11:40 +0800

    New index UI design

--- pages/index.php
+++ pages/index.php
@@ -14,7 +14,7 @@
 
 
     <!--My CSS and JS-->
-    <link href="../css/index.css" rel="stylesheet"/>
+    <link type="text/css" rel="stylesheet" href="../css/index.css"/>
     <!--script src="../javascript/index.js"></script-->
     <title>Document</title>
 </head>
@@ -29,21 +29,21 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block" src="../asset/b.jpg" alt="First slide">
+                        <img class="d-block" src="../asset/pic1.jpg" alt="First slide" style="width: 100%; height: 857px;">
                         <div class="carousel-caption">
-                            <h4 id="announceHead1">Announcement 1</h4>
-                            <p id="context1" class="text-justify">...</p>
+                            <h4 id="announceHead1">Announcement!</h4>
+                            <p id="context1" class="text-justify">For writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity. The most common way to use the sentence is to begin a story. Another option is to include it somewhere in the story. A much more difficult challenge is to use it to end a story. In any of these cases, it forces the writer to think creatively since they have no idea what sentence will appear from the tool.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block img-responsive" src="../asset/b.jpg" alt="Second slide">
+                        <img class="d-block img-responsive" src="../asset/sti.jpg" alt="Second slide" style="width: 100%; height: 857px;">
                         <div class="carousel-caption">
-                            <h4 id="announceHead2">Announcement 2</h4>
-                            <p id="context2" class="text-justify">adssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</p>
+                            <h4 id="announceHead2">Important</h4>
+                            <p id="context2" class="text-justify">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block" src="../asset/b.jpg" alt="Third slide">
+                        <img class="d-block img-responsive" src="../asset/pic3.jpeg" alt="Third slide" style="width: 100%; height: 857px;">
                         <div class="carousel-caption">
                             <h4 id="announceHead3">Announcement 3</h4>
                             <p id="context3" class="text-justify">...</p>
@@ -61,27 +61,37 @@
             </div>
         </div>
 
-        <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4" style="background-color:gray;">
-            <div class="row no-gutters d-flex pt-5 mt-5">
-                <div class="col-sm-12 col-xs-12 col-md-12 pt-5 mt-5">
-                    <h1 class="text-center">Choose one below</h1>
+        <div class="col-sm-4 col-xs-4 col-md-4 col-lg-4 px-2 py-5" style="background-color: #3466AA;">
+            <div class="container pagination-centered text-center mx-auto pt-5 mt-5" style="background-color: #F1F1F1; border-radius: 20px; box-shadow: -2px 2px 18px 4px black;">
+                <div class="row no-gutters d-flex">
+                    <div class="col-sm-12 col-xs-12 col-md-12">
+                        <h1 class="pt-2 mt-2">Entrance Monitoring System</h1>
+                    </div>
                 </div>
-            </div>
+                <div class="row no-gutters d-flex justify-content-center mb-5 px-2 mx-2 pt-5 mt-5">
+                    <div class="col-sm-12 col-xs-12 col-md-12 pt-2 mt-2 d-flex">
+                        <h5 class="ml-2 pl-2 ">Identify yourself as:</h5>
+                    </div>
+                    <div class="col-sm-12 col-xs-12 col-md-12 pt-2 mt-2 d-flex justify-content-center">
+                        <button type="button" class="btn btn-lg mx-auto pl-3" id="myBtn" style="width: 150px; background-color: #3466AA; color:white;">Visitor</button>
+                        <button type="button" class="btn btn-lg mx-auto pr-3" style="width: 150px; background-color:#114084; color:white;">Guardian</button>
+                    </div>
+                    <div class="col-sm-12 col-xs-12 col-md-12 pt-2 mt-2 pb-5 mb-5 d-flex justify-content-center">
 
-            <div class="row no-gutters d-flex justify-content-center pt-5 mt-5">
-                <div class="col-sm-12 col-xs-12 col-md-12 pt-5 mt-5 d-flex justify-content-between">
-                    <button type="button" class="btn btn-lg ml-5 " style="width: 200px; background-color:#3466AA; color:white;">Visitor</button>
-                    <button type="button" class="btn btn-lg mr-5 ml-1" style="width: 200px; background-color:#114084; color:white;">Guardian</button>
-                </div>
-            </div>
+                        <h5 class="ml-2 pl-2" style="font-size: 13px;">Don't have any account? <a class="text-primary" href="../page/signup.php">Click here</a></h5>
 
-            <div class="row no-gutters pt-5 mt-5">
-                <div class="d-flex justify-content-center col-sm-12 col-xs-12 col-md-12 pt-5 mt-5">
-                    <h5><a class="text-primary" href="../page/signup.php">Just click here</a> to learn more.</h5>
+                        
+                    </div>
                 </div>
             </div>
+            
         </div>
     </div>
-    
+
+    <div class="row no-gutters align-items-end" style="background-color: #9C9C9C;">
+        <div class="d-flex justify-content-center col-sm-12 col-xs-12 col-md-12 pb-2 mb-2 pt-3 mt-3">
+            <h5><a class="text-primary" href="../page/signup.php">Just click here</a> to learn more.</h5>
+        </div>
+    </div>
 </body>
 </html>
