002a85d7ccea1152d10db2e130a07bb376865154
changco0987 <williamchangco26@gmail.com>
2022-08-01 20:43:52 +0800

    add few changes in UI

--- pages/index.php
+++ pages/index.php
@@ -16,7 +16,9 @@
     <!--My CSS and JS-->
     <link type="text/css" rel="stylesheet" href="../css/index.css"/>
     <!--script src="../javascript/index.js"></script-->
-    <title>Document</title>
+
+    <link rel="icon" href="../asset/icon.png">
+    <title>Entrance Monitoring sys - Home page</title>
 </head>
 <body>
     <div class="row no-gutters">
@@ -29,21 +31,21 @@
                 </ol>
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img class="d-block" src="../asset/pic1.jpg" alt="First slide" style="width: 100%; height: 857px;">
+                        <img class="d-block" src="../asset/pic1.jpg" alt="First slide" style="width: 100%; height: 874px;">
                         <div class="carousel-caption">
                             <h4 id="announceHead1">Announcement!</h4>
                             <p id="context1" class="text-justify">For writers, a random sentence can help them get their creative juices flowing. Since the topic of the sentence is completely unknown, it forces the writer to be creative when the sentence appears. There are a number of different ways a writer can use the random sentence for creativity. The most common way to use the sentence is to begin a story. Another option is to include it somewhere in the story. A much more difficult challenge is to use it to end a story. In any of these cases, it forces the writer to think creatively since they have no idea what sentence will appear from the tool.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block img-responsive" src="../asset/sti.jpg" alt="Second slide" style="width: 100%; height: 857px;">
+                        <img class="d-block img-responsive" src="../asset/sti.jpg" alt="Second slide" style="width: 100%; height: 874px;">
                         <div class="carousel-caption">
                             <h4 id="announceHead2">Important</h4>
                             <p id="context2" class="text-justify">If you're visiting this page, you're likely here because you're searching for a random sentence. Sometimes a random word just isn't enough, and that is where the random sentence generator comes into play. By inputting the desired number, you can make a list of as many random sentences as you want or need. Producing random sentences can be helpful in a number of different ways.</p>
                         </div>
                     </div>
                     <div class="carousel-item">
-                        <img class="d-block img-responsive" src="../asset/pic3.jpeg" alt="Third slide" style="width: 100%; height: 857px;">
+                        <img class="d-block img-responsive" src="../asset/pic3.jpeg" alt="Third slide" style="width: 100%; height: 874px;">
                         <div class="carousel-caption">
                             <h4 id="announceHead3">Announcement 3</h4>
                             <p id="context3" class="text-justify">...</p>
@@ -78,7 +80,7 @@
                     </div>
                     <div class="col-sm-12 col-xs-12 col-md-12 pt-2 mt-2 pb-5 mb-5 d-flex justify-content-center">
 
-                        <h5 class="ml-2 pl-2" style="font-size: 13px;">Don't have any account? <a class="text-primary" href="../page/signup.php">Click here</a></h5>
+                        <h5 class="ml-2 pl-2" style="font-size: 13px;">Don't have any account? <a class="text-primary" href="../pages/signup.php">Click here</a></h5>
 
                         
                     </div>
@@ -87,11 +89,16 @@
             
         </div>
     </div>
-
-    <div class="row no-gutters align-items-end" style="background-color: #9C9C9C;">
-        <div class="d-flex justify-content-center col-sm-12 col-xs-12 col-md-12 pb-2 mb-2 pt-3 mt-3">
-            <h5><a class="text-primary" href="../page/signup.php">Just click here</a> to learn more.</h5>
+    <!--Footer Section-->
+    <div class="row no-gutters">
+    <footer class=" text-center text-lg-end fixed-bottom">
+        <div class="d-flex justify-content-center p-3" style="background-color:#9C9C9C;">
+            <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">Contacts</a></u></h5>
+            <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">School History</a></u></h5>
+            <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">About</a></u></h5>
         </div>
+    </footer>
     </div>
+
 </body>
 </html>
--- pages/signup.php
+++ pages/signup.php
@@ -12,9 +12,67 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <title>Document</title>
+
+    <!--My CSS and JS-->
+    <link type="text/css" rel="stylesheet" href="../css/signup.css"/>
+    <!--script src="../javascript/index.js"></script-->
+
+    <link rel="icon" href="../asset/icon.png">
+    <title>Entrance Monitoring sys - Sign up</title>
 </head>
 <body>
-    
+    <div class="row myRow mt-5 pt-5">
+        <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 my-5 py-5">
+            <div class="container mt-5 pt-5 px-2 mx-2">
+                <div class="d-flex justify-content-center">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <h1>Sign up</h1>
+                            <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+                            <div class="row">
+                                <div class="col-sm-6 col-xs-6 col-md-6 col-lg-6  pt-2 mt-2">
+                                    <input type="text" class="form-control no-border" id="fnameTb" name="fnameTb" placeholder="First name" maxlength="50" required>
+                                </div>
+                                <div class="col-sm-6 col-xs-6 col-md-6 col-lg-6 pt-2 mt-2">
+                                    <input type="text" class="form-control" id="lnameTb" name="lnameTb" placeholder="Last name" maxlength="50" required>
+                                </div>
+                            </div>
+                            <div class="row pt-2 mt-2">
+                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+                                    <input type="text" class="form-control" id="usernameTb" name="usernameTb" placeholder="Username" maxlength="20" required>
+                                </div>
+                            </div>
+                            <div class="row pt-2 mt-2">
+                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+                                    <input type="password" class="form-control" id="passwordTb" name="passwordTb" placeholder="Password" minlength="8" maxlength="20" required>
+                                </div>
+                            </div>
+                            <div class="row pt-2 mt-2">
+                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+                                    <input type="email" class="form-control no-border" id="emailTb" name="emailTb" placeholder="Email" maxlength="80" required>
+                                </div>
+                            </div>
+                            <div class="row pt-2 mt-2">
+                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+                                    <button type="submit" class="form-control btn btn-primary" id="submitBtn">Submit</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!--Footer Section-->
+    <div class="row">
+        <footer class=" text-center text-lg-end fixed-bottom">
+            <div class="d-flex justify-content-center p-3" style="background-color:#9C9C9C;">
+                <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">Contacts</a></u></h5>
+                <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">School History</a></u></h5>
+                <h5><u><a class="text-secondary px-2" href="../page/signup.php" target="_blank">About</a></u></h5>
+            </div>
+        </footer>
+    </div>
 </body>
 </html>
