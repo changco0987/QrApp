a1f10e480cbc6b60eb2ddc084c2bc7188d1670e6
changco0987 <williamchangco26@gmail.com>
2022-08-06 22:21:39 +0800

    Changing button to anchor to eliminate of using js

--- pages/visitorLogin.php
+++ pages/visitorLogin.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSS only -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -22,7 +22,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 
-    <script src="javascript/linkJs.js"></script>
+    <script src="../javascript/linked.js"></script>
     <link rel="stylesheet" href="../css/login.css">
 
     <link rel="icon" href="../asset/icon.png">
