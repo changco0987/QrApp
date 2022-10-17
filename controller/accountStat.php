92a978b3a8bbaa07f02d7f1c0d9ae8521280197d
changco0987 <williamchangco26@gmail.com>
2022-10-17 19:52:12 +0800

    added: delete and change stat

--- controller/accountStat.php
+++ controller/accountStat.php
@@ -1,7 +1,11 @@
 <?php
     include_once '../db/connection.php';
+
     include_once '../db/tb_visitor.php';
     include_once '../model/visitorModel.php';
+
+    include_once '../db/tb_guardian.php';
+    include_once '../model/guardianModel.php';
 
 
     
@@ -9,17 +13,21 @@
     {
         if($_POST['accType']=='visitor')
         {
-            $student = new visitorModel();
-            $student->setId($_POST['idTb']);
-            $student->setStatus($_POST['statusTb']);
+            $data = new visitorModel();
+            $data->setId($_POST['idTb']);
+            $data->setStatus($_POST['statusTb']);
     
-            UpdateAccountVisitor($conn,$student);
+            UpdateAccountVisitor($conn,$data);
             echo '<script> localStorage.setItem("visitorMsg",1); window.location = "../admin/admin_visitor.php";</script>';//success message
         }
         else if($_POST['accType']=='guardian')
         {
-
-
+            $data = new guardianModel();
+            $data->setId($_POST['idTb']);
+            $data->setStatus($_POST['statusTb']);
+    
+            UpdateAccountGuardian($conn,$data);
+            echo '<script> localStorage.setItem("visitorMsg",1); window.location = "../admin/admin_guardian.php";</script>';//success message
         }
     }
     else
--- controller/deleteAccount.php
+++ controller/deleteAccount.php
@@ -1,24 +1,34 @@
 <?php
     include_once '../db/connection.php';
+
     include_once '../db/tb_visitor.php';
     include_once '../model/visitorModel.php';
+
+    include_once '../db/tb_guardian.php';
+    include_once '../model/guardianModel.php';
 
 
     if(isset($_POST['idTb']))
     {
         if($_POST['accType'] == 'visitor')
         {
-            $student = new visitorModel();
-            $student->setId($_POST['idTb']);
+            $data = new visitorModel();
+            $data->setId($_POST['idTb']);
     
-            DeleteAccountVisitor($conn,$student);
+            DeleteAccountVisitor($conn,$data);
     
             //header('Location: ../admin/dashboard.php');
             echo '<script> localStorage.setItem("visitorMsg",4); window.location = "../admin/admin_visitor.php";</script>';//success message
         }
         else if($_POST['accType'] == 'guardian')
         {
-
+            $data = new guardianModel();
+            $data->setId($_POST['idTb']);
+    
+            DeleteAccountGuardian($conn,$data);
+    
+            //header('Location: ../admin/dashboard.php');
+            echo '<script> localStorage.setItem("visitorMsg",4); window.location = "../admin/admin_guardian.php";</script>';//success message
         }
    
     }
