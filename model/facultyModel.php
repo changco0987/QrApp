de52b5024c4056a2074cc43b1f48654a516211eb
changco0987 <williamchangco26@gmail.com>
2022-11-02 17:22:20 +0800

    added: faculty model

---
+++ model/facultyModel.php
@@ -0,0 +1,102 @@
+<?php
+    class visitorModel
+    {
+        private $id;
+        private $firstname;
+        private $lastname;
+        private $department;
+        private $imageName;
+        private $status;
+        private $gateStat;
+        private $dtrId;
+
+
+        public function getId()
+        {
+            return $this->id;   
+        }
+
+        public function setId($id)
+        {
+            $this->id = $id;
+        }
+
+        
+        public function getFirstname()
+        {
+            return $this->firstname;   
+        }
+
+        public function setFirstname($firstname)
+        {
+            $this->firstname = $firstname;
+        }
+
+        public function getLastname()
+        {
+            return $this->lastname;   
+        }
+
+        public function setLastname($lastname)
+        {
+            $this->lastname = $lastname;
+        }
+
+
+        public function getDepartment()
+        {
+            return $this->department;   
+        }
+
+        public function setDepartment($department)
+        {
+            $this->department = $department;
+        }
+
+
+        public function getImageName()
+        {
+            return $this->imageName;   
+        }
+
+        public function setImageName($imageName)
+        {
+            $this->imageName = $imageName;
+        }
+
+
+        public function getStatus()
+        {
+            return $this->status;   
+        }
+
+        public function setStatus($status)
+        {
+            $this->status = $status;
+        }
+
+
+        public function getGateStat()
+        {
+            return $this->gateStat;   
+        }
+
+        public function setGateStat($gateStat)
+        {
+            $this->gateStat = $gateStat;   
+        }
+
+
+        public function getDtrId()
+        {
+            return $this->dtrId;   
+        }
+
+        public function setDtrId($dtrId)
+        {
+            $this->dtrId = $dtrId;
+        }
+
+    }
+
+?>
