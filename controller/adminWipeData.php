e02f387778a855ac367516e01b0ad2946483f6a1
changco0987 <williamchangco26@gmail.com>
2022-11-04 11:41:45 +0800

    added: checking session var in wipedata

--- controller/adminWipeData.php
+++ controller/adminWipeData.php
@@ -5,16 +5,20 @@
     date_default_timezone_set('Asia/Manila'); 
     session_start();
 
-    $log = new logsModel();
-
-
-    //create log
-    $log->setActivity('Sign-out');
-    $log->setIpAdd();
-    $log->setAccType('Administrator');
-    $log->setCreator($_SESSION['adminNameTb']);
-
-    CreateLog($conn,$log);
+    //This will check if the session is set to avoid error
+    if(isset($_SESSION['adminNameTb']))
+    {
+        $log = new logsModel();
+    
+    
+        //create log
+        $log->setActivity('Sign-out');
+        $log->setIpAdd();
+        $log->setAccType('Administrator');
+        $log->setCreator($_SESSION['adminNameTb']);
+    
+        CreateLog($conn,$log);
+    }
     
     $helper = array_keys($_SESSION);
     foreach ($helper as $key)
--- controller/wipedata.php
+++ controller/wipedata.php
@@ -5,16 +5,20 @@
     date_default_timezone_set('Asia/Manila'); 
     session_start();
 
-    $log = new logsModel();
-
-
-    //create log
-    $log->setActivity('Sign-out');
-    $log->setIpAdd();
-    $log->setAccType($_SESSION['accType']);
-    $log->setCreator($_SESSION['username']);
-
-    CreateLog($conn,$log);
+    //This will check if the session is set to avoid error
+    if(isset($_SESSION['accType']) && isset($_SESSION['username']))
+    {
+        $log = new logsModel();
+    
+    
+        //create log
+        $log->setActivity('Sign-out');
+        $log->setIpAdd();
+        $log->setAccType($_SESSION['accType']);
+        $log->setCreator($_SESSION['username']);
+    
+        CreateLog($conn,$log);
+    }
     
     $helper = array_keys($_SESSION);
     foreach ($helper as $key)
