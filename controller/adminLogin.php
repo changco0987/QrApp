4d0a0d14bf24d5fc882a4045647035092d2b6f75
changco0987 <williamchangco26@gmail.com>
2022-11-10 14:59:16 +0800

    added: log in attempt logs in admin

--- controller/adminLogin.php
+++ controller/adminLogin.php
@@ -2,6 +2,9 @@
     include_once '../db/connection.php';
     include_once '../db/tb_admin.php';
     include_once '../model/adminModel.php';
+    
+    include_once '../db/tb_logs.php';
+    include_once '../model/logsModel.php';    
 
     if(isset($_POST['adminNameTb']))
     {
@@ -10,24 +13,50 @@
         $data->setPassword($_POST['adminPasswordTb']);
         $result = ReadAdmin($conn,$data);//This is will be the container of the admin data
         
+        $log = new logsModel();
         while($row = mysqli_fetch_assoc($result))
         {
 
             if($row['username']==$data->getUsername() && $row['password']==$data->getPassword())
             {
+   
+                //create log
+                $log->setActivity('log-in');
+                $log->setIpAdd();
+                $log->setAccType('Administrator');
+                $log->setCreator($row['username']);
+            
+                CreateLog($conn,$log);
+
                 session_start();
                 $_SESSION['adminNameTb'] = $row['username'];
                 header("Location: ../admin/dashboard.php");
+                exit;
             }
             else
             {
+                //create log
+                $log->setActivity('log-in attempt: incorrect password');
+                $log->setIpAdd();
+                $log->setAccType('Administrator');
+                $log->setCreator($row['username']);
+            
+                CreateLog($conn,$log);
                 //Throws back to the login page and show "Username or Password is incorrect"
-               echo '<script> localStorage.setItem("state",1); window.location = "../admin.php";</script>';
-               exit;
+                echo '<script> localStorage.setItem("state",1); window.location = "../admin.php";</script>';
+                exit;
             }
 
         }
 
+        
+        //create log
+        $log->setActivity('log-in attempt: incorrect username and password');
+        $log->setIpAdd();
+        $log->setAccType('Administrator');
+        $log->setCreator('Unknown');
+    
+        CreateLog($conn,$log);
         //Throws back to the login page and show "This account is not existed"
        echo '<script> localStorage.setItem("state",2); window.location = "../admin.php";</script>';
        exit;
--- model/logsModel.php
+++ model/logsModel.php
@@ -48,8 +48,26 @@
 
         public function setIpAdd()
         {
+            $ip = '';
+
+               //whether ip is from the share internet  
+            if(!empty($_SERVER['HTTP_CLIENT_IP'])) 
+            {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+            }  
+            //whether ip is from the proxy  
+            elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+            {  
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+            }  
+            //whether ip is from the remote address  
+            else
+            {  
+                $ip = $_SERVER['REMOTE_ADDR'];  
+            } 
+            
             //$this->ipAdd = getHostByName(getHostName());
-            $this->ipAdd = $_SERVER['REMOTE_ADDR'];
+            $this->ipAdd = $ip;
         }
 
         public function getAccType()
