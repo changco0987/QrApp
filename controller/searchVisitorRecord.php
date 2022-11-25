96f3ba471966fcc89ba5e9717d88cca1280076e7
changco0987 <williamchangco26@gmail.com>
2022-11-25 15:42:53 +0800

    applied new search algo for visitor record

---
+++ controller/searchVisitorRecord.php
@@ -0,0 +1,87 @@
+<?php
+    include_once '../db/connection.php';
+    include_once '../db/tb_visitor.php';
+    include_once '../model/visitorModel.php';
+
+    include_once '../db/tb_dtr.php';
+    include_once '../model/dtrModel.php';
+    
+    $search = $_GET['search'];
+  
+    $data = new visitorModel();
+    $dtr = new dtrModel();
+
+    $dtrData = ReadDtr($conn,$dtr);
+    $rowCount = 1;
+    $changeColor = 0;//This is to change the color
+
+    $findings = "";
+    while($dtrRow = mysqli_fetch_assoc($dtrData))
+    {
+        if($dtrRow['accType'] == 'visitor')
+        {
+            
+            $data->setId($dtrRow['dataId']);
+            $result = ReadAccountVisitor($conn,$data);
+            while($row = mysqli_fetch_assoc($result))
+            {
+                if(strtolower($row['firstname']) == strtolower($search) ||
+                    str_contains(strtolower($row['firstname']),strtolower($search)))
+                {
+
+                    if($changeColor==0)
+                    {
+                        $findings = $findings. "<tr style='background-color:#82B7DC;'>";
+                        
+                        $changeColor++;
+                    }                   
+                    else if($changeColor==1)
+                    {
+                        $findings = $findings. "<tr style='background-color:#6aa9d5;'>";
+                        
+                        $changeColor=0;
+                    }
+                        $findings = $findings. "<td>$rowCount</td>".
+                            "<td>".$row['firstname'].' '.$row['lastname']."</td>".
+                            "<td>".$dtrRow['temperature']."</td>".
+                            "<td>".$dtrRow['time_in']."</td>".
+                            "<td>".$dtrRow['time_out']."</td>".
+                        "</tr>";
+                        $rowCount++;
+                }
+                else if(strtolower($row['lastname']) == strtolower($search) ||
+                    str_contains(strtolower($row['lastname']),strtolower($search)))
+                {
+
+                    if($changeColor==0)
+                    {
+                        $findings = $findings. "<tr style='background-color:#82B7DC;'>";
+                        
+                        $changeColor++;
+                    }                   
+                    else if($changeColor==1)
+                    {
+                        $findings = $findings. "<tr style='background-color:#6aa9d5;'>";
+                        
+                        $changeColor=0;
+                    }
+                        $findings = $findings. "<td>$rowCount</td>".
+                            "<td>".$row['firstname'].' '.$row['lastname']."</td>".
+                            "<td>".$dtrRow['temperature']."</td>".
+                            "<td>".$dtrRow['time_in']."</td>".
+                            "<td>".$dtrRow['time_out']."</td>".
+                        "</tr>";
+                        $rowCount++;
+                }
+
+            }
+        }
+    }
+
+    $response = $findings;
+    echo $response;
+
+       
+    
+
+?>
