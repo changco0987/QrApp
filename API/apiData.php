32facf6ca266ebb87497ab370ab98920877d20c5
changco0987 <williamchangco26@gmail.com>
2022-11-27 20:12:09 +0800

    Change title in Exam view

--- API/apiData.php
+++ API/apiData.php
@@ -6,7 +6,7 @@
     //Important API variables
     $key = "a6cd8649fbb860dfc09471b8eedd0d2a0badb42f";//API Key
     $device = 178;//Device ID
-    $sim = 2;//SIM slot
+    $sim = 1;//SIM slot
     $priority = 1;//prioritize
     $group = 265;//Group ID
     //$phone = "+639495029072";
