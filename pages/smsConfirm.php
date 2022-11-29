ab6bae75cda783d92d4b69dcc3b06beb8e90fb49
changco0987 <williamchangco26@gmail.com>
2022-11-29 20:15:43 +0800

    added: SMS confirmation page in signup

--- pages/smsConfirm.php
+++ pages/smsConfirm.php
@@ -1,3 +1,17 @@
+<?php
+    session_start();
+
+    
+    date_default_timezone_set('Asia/Manila');
+
+    if(!isset($_POST['submitBtn']))
+    {
+        header('Location: ../index.php');
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,20 +28,23 @@
     <!--Bootstrap icon--> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
-    <!--My CSS and JS-->
-    <!--link type="text/css" rel="stylesheet" href="../css/signup.css"/-->
-    <!--script src="../javascript/index.js"></script-->
+
+    <script src="../javascript/linked.js"></script>
+    <link rel="stylesheet" href="../css/login.css">
+
     <style>
-        body{
-    background-color: #3466AA;
-    font-family: Comic Sans MS, Comic Sans;
-}
-label{
+ 
+
+ label{
     font-size: 12px;
     color: #234471;
+    font-weight: bold;
 }
 
-.containerForm{
+
+ @media screen and (max-height: 850px) {
+
+.container {
     background-color: #f1f1f1; /* Fallback color */
     color: black;
     font-weight: bold;
@@ -35,40 +52,9 @@ label{
     top: 50%;
     left: 50%;
     transform: translateX(-50%) translateY(-50%);
-    height: 600px;
-    width: 650px;
-    padding: 20px;
-    text-align: center;
-    border-radius: 15px;
-    box-shadow: -1px 1px 20px 6px black;
-    
-}
-
-.myRow{
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    transform: translateX(-50%) translateY(-40%);
-}
-
-.form-control{
-    border: 0;
-}
-
-
-@media screen and (max-height: 850px) {
-
-.containerForm {
-    background-color: #f1f1f1; /* Fallback color */
-    color: black;
-    font-weight: bold;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    height: 610px;
-    width: 400px;
-    padding: 15px;
+    height:max-content;
+    width:max-content;
+    padding: 5px;
     text-align: center;
     border-radius: 15px;
     box-shadow: -1px 1px 20px 6px black;
@@ -80,16 +66,16 @@ label{
     background-color: #f1f1f1;
     font-family: "Bahnschrift", Times, serif;
 }
-.containerForm {
+.container {
     background-color: #f1f1f1; /* Fallback color */
     color: black;
     font-weight: bold;
     position: absolute;
-    top: 35%;
+    top: 50%;
     left: 50%;
-    transform: translateX(-50%) translateY(-35%);
-    height: 750px;
-    width: 385px;
+    transform: translateX(-50%) translateY(-50%);
+    height:max-content;
+    width:max-content;
     padding: 5px;
     text-align: center;
     border-radius: 0px;
@@ -97,22 +83,45 @@ label{
 }
 }
 
-/* S20 Ultra and iphone6/7/8 Plus*/
 @media screen and (max-width: 450px) {
     body{  
     background-color: #f1f1f1;
+    font-family: "Bahnschrift", Times, serif;
 }
-.containerForm {
+.container {
     background-color: #f1f1f1; /* Fallback color */
     color: black;
     font-weight: bold;
     position: absolute;
-    top: 40%;
+    top: 50%;
     left: 50%;
-    transform: translateX(-50%) translateY(-40%);
-    height: 700px;
-    width: 385px;
+    transform: translateX(-50%) translateY(-50%);
+    height:max-content;
+    width:max-content;
     padding: 5px;
+    text-align: center;
+    border-radius: 0px;
+    box-shadow: none;
+}
+}
+
+
+@media screen and (max-width: 360px) {
+    body{  
+    background-color: #f1f1f1;
+    font-family: "Bahnschrift", Times, serif;
+}
+.container {
+    background-color: #f1f1f1; /* Fallback color */
+    color: black;
+    font-weight: bold;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    height: 890px;
+    width: 350px;
+    padding: 20px;
     text-align: center;
     border-radius: 0px;
     box-shadow: none;
@@ -120,27 +129,30 @@ label{
 }
 
 /* S8+ */
-@media screen and (max-width: 360px) {
-    body{  
+@media screen and (max-width: 375px) {
+body{  
     background-color: #f1f1f1;
-    font-family: "Bahnschrift", Times, serif;
 }
-.containerForm {
+.container {
     background-color: #f1f1f1; /* Fallback color */
     color: black;
     font-weight: bold;
     position: absolute;
-    top: 35%;
+    top: 50%;
     left: 50%;
-    transform: translateX(-50%) translateY(-35%);
-    height: 890px;
-    width: 350px;
+    transform: translateX(-50%) translateY(-50%);
+    height:max-content;
+    width:max-content;
     padding: 5px;
     text-align: center;
     border-radius: 0px;
     box-shadow: none;
 }
+footer * {
+    font-size: 17px;
 }
+
+}  
 
 /* S9 */
 @media screen and (max-width: 320px) {
@@ -148,34 +160,31 @@ body{
     background-color: #f1f1f1;
     font-family: "Bahnschrift", Times, serif;
 }
-.containerForm {
+.container {
     background-color: #f1f1f1; /* Fallback color */
     color: black;
     font-weight: bold;
     position: absolute;
-    top: 25%;
+    top: 50%;
     left: 50%;
-    transform: translateX(-50%) translateY(-25%);
-    height: 750px;
-    width: 300px;
+    transform: translateX(-50%) translateY(-50%);
+    height:max-content;
+    width:max-content;
     padding: 5px;
     text-align: center;
     border-radius: 0px;
     box-shadow: none;
 }
-
 footer * {
     font-size: 17px;
 }
 
 }
-
     </style>
     <link rel="icon" href="../asset/qr.png">
-    <title>Entrance Monitoring sys - Sign up</title>
+    <title>Entrance Monitoring sys - Confirm Number</title>
 </head>
 <body>
-    
     <!-- Image and text Header-->
     <nav class="navbar navbar-light" style="background-color: #114084;">
         <a class="navbar-brand" href="#" style="font-weight:bold; color: whitesmoke; text-shadow: 1px 1px #1C1C1C;">
@@ -183,67 +192,50 @@ footer * {
             QR <small style="color: whitesmoke; text-shadow: 1px 1px #1C1C1C;">Entrance Monitoring System</small>
         </a>
      </nav>
+    
+
+
     <div class="row myRow mt-5 pt-5 mx-auto">
         <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 my-5 py-5">
-            <div class="containerForm">
-                <div class="d-flex justify-content-center">
-                    <form action="../pages/smsConfirm.php" method="post" enctype="multipart/form-data">
-                        <input type="hidden" name="accType" value="visitor">
-                        <div class="form-group mt-1 pt-1">
+            <div class="container">
+                <div class="d-flex justify-content-center my-4 py-1">
+                    <form action="../controller/signup.php" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="accType" value="<?php echo $_POST['accType'];?>">
+                        <input type="hidden" name="fnameTb" value="<?php echo $_POST['fnameTb'];?>">
+                        <input type="hidden" name="lnameTb" value="<?php echo $_POST['lnameTb'];?>">
+                        <input type="hidden" name="usernameTb" value="<?php echo $_POST['usernameTb'];?>">
+                        <input type="hidden" name="passwordTb" value="<?php echo $_POST['passwordTb'];?>">
+                        <input type="hidden" name="addressTb" value="<?php echo $_POST['addressTb'];?>">
+                        <input type="hidden" name="contactTb" value="<?php echo $_POST['contactTb'];?>">
+                        <?php
+                            if($_POST['accType'] == 'guardian')
+                            {
+                                ?>
+                                    <input type="hidden" name="studentidTb" value="<?php echo $_POST['studentidTb'];?>">
+                                    <input type="hidden" name="notifCheckbox" value="<?php echo $_POST['notifCheckbox'];?>">
+                                <?php
+                            }
+                        ?>
+                        <div class="form-group">
                             <center>
-                                <h1>Sign up <small style="font-size: 20px; font-weight:bold; color:#3466AA;">as visitor</small></h1>
+                            <h2>Enter your OTP</h2>
                             </center>
-                            <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+                            <hr style="height:2px; border-width:0;background-color: #3466AA">
                         </div>
                         <div class="form-group">
                             <div class="row">
-                                <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 pt-2 mt-2">
-                                    <label class="d-flex align-items-start" for="fnameTb">First name</label>
-                                    <input type="text" class="form-control no-border" id="fnameTb" name="fnameTb" placeholder="Ex. Marie" maxlength="50" required>
-                                </div>
-                                <div class="col-sm-12 col-xs-12 col-md-6 col-lg-6 pt-2 mt-2">
-                                    <label class="d-flex align-items-start" for="lnameTb">Last name</label> 
-                                    <input type="text" class="form-control" id="lnameTb" name="lnameTb" placeholder="Ex. Cruz" maxlength="50" required>
+                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
+                                    <label class="d-flex align-items-start" for="otpTb">OTP code</label>
+                                    <input type="text" class="form-control form-control-sm" id="otpTb" name="otpTb" placeholder="Ex. 12345678" minlength="8" maxlength="8" required>
+                                    <small style="color:red; font-size:12px;">Never share your OTP nor inputting in other web sites</small>
                                 </div>
                             </div>
                         </div>
+
                         <div class="form-group">
-                            <div class="row pt-1 mt-1">
+                            <div class="row">
                                 <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-                                    <label class="d-flex align-items-start" for="usernameTb">Username</label>
-                                    <input type="text" class="form-control" id="usernameTb" name="usernameTb" placeholder="Ex. Marie0123" maxlength="20" required>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row pt-1 mt-1">
-                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-                                    <label class="d-flex align-items-start" for="passwordTb">Password</label>
-                                    <input type="password" class="form-control" id="passwordTb" name="passwordTb" placeholder="Ex. CMarie123" minlength="8" maxlength="20" required>
-                                    <small class="d-flex align-items-start" style="color:red;">Use at least 8 or up to 15 characters for your password </small>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row pt-1 mt-1">
-                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-                                    <label class="d-flex align-items-start" for="addressTb">Address</label>
-                                    <input type="text" class="form-control no-border" id="addressTb" name="addressTb" placeholder="Ex. 2123 home st." maxlength="90" required>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row pt-1 mt-1">
-                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-                                    <label class="d-flex align-items-start" for="contactTb">Contact Number</label>
-                                    <input type="text" class="form-control no-border" id="contactTb" name="contactTb" placeholder="Ex. 092X-XXX-XXXX" maxlength="11" required>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="row pb-5 mb-5">
-                                <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12">
-                                    <button type="submit" class="form-control btn" id="submitBtn" name="submitBtn" style="background-color: #3466AA; color:white;">Submit</button>
+                                    <button type="submit" class="form-control btn btn-sm" name="submitBtn" id="submitBtn" style="background-color: #3466AA; color:white;">Continue</button>
                                 </div>
                             </div>
                         </div>
@@ -264,6 +256,7 @@ footer * {
     </footer>
     </div>
 
+
     <!-- Alert message container-->
     <div id="alertBox" class="alert alert-danger alert-dismissible fade show" role="alert" style="display:block ;">
         <strong id="errorMsg">Holy guacamole!</strong>
@@ -271,17 +264,28 @@ footer * {
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
+    <!-- Alert message container-->
+    <div id="successBox" class="alert alert-success alert-dismissible fade show" role="alert" style="display:block;">
+        <strong id="successMsg"></strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+    
+    
 </body>
+
     <!--alert message script-->
     <script>
+        document.getElementById('successBox').style.display = 'none';
         document.getElementById('alertBox').style.display = 'none';
-        var successSignal = localStorage.getItem('state');
+        var successSignal = localStorage.getItem('otpMsg');
 
         if(successSignal==1)
         {
             //if incorrect password
             document.getElementById('alertBox').style.display = 'block';
-            document.getElementById('errorMsg').innerHTML = 'Username is already existed';
+            document.getElementById('errorMsg').innerHTML = 'Username does not exist!';
             console.log("okay");
 
         }
@@ -289,7 +293,7 @@ footer * {
         {
             //if email is already taken
             document.getElementById('alertBox').style.display = 'block';
-            document.getElementById('errorMsg').innerHTML = 'You cannot use white spaces';
+            document.getElementById('errorMsg').innerHTML = 'Sorry, this account does not exist';
             console.log("okay");
         }
         else if(successSignal==3)
@@ -299,8 +303,22 @@ footer * {
             document.getElementById('errorMsg').innerHTML = "Password doesn't match!";
             console.log("okay");
         }
+        else if(successSignal==4)
+        {
+            //if password doesn't matched
+            document.getElementById('successBox').style.display = 'block';
+            document.getElementById('successMsg').innerHTML = "Account Created Successfully!";
+            console.log("okay");
+        }
+        else if(successSignal==5)
+        {
+            //if the account is locked
+            document.getElementById('alertBox').style.display = 'block';
+            document.getElementById('errorMsg').innerHTML = "This account is locked, please contact the admin";
+            console.log("okay");
+        }
 
         //To make signl back to normmal and to prevent for the success page to appear every time the page was reload or refresh
-        localStorage.setItem('state',0);
+        localStorage.setItem('otpMsg',0);
     </script>
 </html>
